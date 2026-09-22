92366d1398a989413011f4ac1ca086d2e6bfd288
Minecraft_goose <18540120423@163.com>
2026-09-22 12:41:44 +0800

    Delete sites directory

--- sites/index.js
+++
@@ -1,8 +0,0 @@
-// ===== 站点模块导出 =====
-
-export * from './create.js';
-export * from './update.js';
-export * from './delete.js';
-export * from './my-sites.js';
-export * from './file.js';
-export * from './serve.js';
